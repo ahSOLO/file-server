8a1bfb0dbae3e91bf39aebfa8396886d5f09ba28
Andrew Han <junmenghan@gmail.com>
2021-01-14 05:59:16 +0000

    stream now works for files of all type

--- server.js
+++ server.js
@@ -2,35 +2,24 @@ const net = require('net');
 const fs = require('fs');
 const path = require('path');
 const { pipeline } = require('stream');
-let stream_in_progress = false;
 
 const server = net.createServer();
 
 server.on('connection', (client) => {
   console.log('New client connected!');
-  client.setEncoding('utf8'); // interpret data as text
   client.on('data', (filename) => {
-    console.log('File Requested:', filename)
+    console.log('File Requested:', filename.toString())
     console.log("Sending Filename");
-    client.write("_EXTNAME_" + path.extname(filename));
-    if (stream_in_progress === false) {
-      stream_in_progress = true;
-      const fileStream = fs.createReadStream(filename);
-      pipeline(
-        fileStream,
-        client,
-        (err) => {
-          if (err) {console.error(err)};
-          console.log('File transfer done');
-          stream_in_progress = false;
-        }
-      );
-    }
-    // fs.readFile(filename, (err, data) => {
-    //   if (err) throw err;
-    //   console.log("Sending File");
-    //   client.write(data);
-    // });
+    client.write("_EXTNAME_" + path.extname(filename.toString()));
+    const fileStream = fs.createReadStream(filename.toString());
+    pipeline(
+      fileStream,
+      client,
+      (err) => {
+        if (err) {console.error(err)};
+        console.log('File transfer done');
+      }
+    );
   });
 });
 
